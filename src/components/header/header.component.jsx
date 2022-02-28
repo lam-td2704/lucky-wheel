10b5ba7362b3580a8d6022ad0bed979f0b45ee8c
TRAN DUC LAM <t-lam@plott.co.jp>
2022-02-28 15:11:43 +0700

    added Cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import "./header.styles.scss";
 
 import { ReactComponent as Logo } from "../../assets/nobugs.svg";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/home">
@@ -40,13 +42,20 @@ const Header = ({ currentUser }) => {
             {currentUser.displayName}
           </div>
         ) : null}
+
+        <div className="option">
+          <CartIcon />
+        </div>
+
+        {hidden ? null : <CartDropdown />}
       </div>
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,6 @@
+import { cartTypes } from "./cart.types";
+const toggleCartHidden = () => ({
+  type: cartTypes.TOGGLE_CART_HIDDEN,
+});
+
+export default toggleCartHidden;
