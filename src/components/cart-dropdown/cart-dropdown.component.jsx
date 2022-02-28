dba30d03bac2de9e263775b72a354e27532e5e02
TRAN DUC LAM <t-lam@plott.co.jp>
2022-02-28 16:59:48 +0700

    update cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-import "./cart-dropdown.styles.scss";
 
-import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <h1 style={{textAlign: 'center'}}>Cart is empty</h1>
+        )}
       </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,6 +4,8 @@ import SHOP_DATA from "./shop.data.js";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
+import "./shop.styles.scss";
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
