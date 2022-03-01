f82106ecbd0ebcb918e924c4ecc7bc6f24d62bdf
TRAN DUC LAM <t-lam@plott.co.jp>
2022-03-01 14:31:34 +0700

    add config firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-const config = {
-  apiKey: "AIzaSyAJGB4C6ImGkuzfDxKHB1gBnwFh-8xFcBc",
-  authDomain: "crwn-clothing-81621.firebaseapp.com",
-  projectId: "crwn-clothing-81621",
-  storageBucket: "crwn-clothing-81621.appspot.com",
-  messagingSenderId: "872196447615",
-  appId: "1:872196447615:web:5fa3b3a8eebdc8a26cce4a",
-  measurementId: "G-X36T5TD9K1",
-};
+import {config} from '../firebase.config';
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
